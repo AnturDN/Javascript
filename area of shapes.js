9d0eb6c62c78eb3034ae33e681446884fc2ff30a
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-01-13 00:11:37 +0600

    Add files via upload

---
+++ area of shapes.js
@@ -0,0 +1,11 @@
+
+/* var base = parseFloat(("Enter base = "));
+var height = parseFloat(prompt("Enter height = ")); */ //for float values.
+
+var base = prompt("Enter base = ");
+var height = prompt("Enter height = ");
+
+var area = base * height;
+
+document.write("Area = "+area);
+
