c034b6725227998110a3b794b30a639a6cb054d6
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-02-05 00:17:45 +0600

    Add files via upload

---
+++ data object and date methods.js
@@ -0,0 +1,22 @@
+
+var date = new Date();
+document.write(date + "<br>");
+
+var year = date.getFullYear();
+document.write(year + "<br>");
+
+var month = date.getMonth();
+document.write(month + "<br>");// its index number starts with 0, like January = 0, February = 1, and so on
+
+var currentDate = date.getDate();
+document.write(currentDate + "<br>");
+
+var currentDay = date.getDay();
+document.write(currentDay + "<br>"); // its index starts with 0, like Sunday = 0, Monday = 1,and so on
+
+var currentHour = date.getHours();
+document.write(currentHour + "<br>");
+
+var currentMinutes = date.getMinutes();
+document.write(currentMinutes + "<br>");
+
