9886eaef5e497931086762c05a2390116daa4261
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-02-07 01:11:01 +0600

    Add files via upload

---
+++ DOM drag event.js
@@ -0,0 +1,31 @@
+// DragEvent Object
+
+// ondragstart
+// ondrag
+// ondragend
+// ondragenter
+// ondragleave
+// ondragover
+// ondrop
+
+
+const div = document.querySelector("div");
+const p = document.querySelector("p");
+
+p.addEventListener('dragstart', function(event){
+    event.dataTransfer.setData("Text", event.target.id);//we are sending the id of the P to the div
+});
+
+div.addEventListener("dragover", function(event){
+    event.preventDefault();
+});
+
+div.addEventListener("drop", function(event){
+    let id = event.dataTransfer.getData("Text");
+    console.log(id);
+    div.appendChild(document.getElementById(id));
+    event.preventDefault();
+});
+
+
+
