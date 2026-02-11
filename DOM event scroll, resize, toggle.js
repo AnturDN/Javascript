7f08745c32458f771e0be3e81dabc44278c01b34
Antur <anturde21@gmail.com>
2026-02-11 11:45:46 +0600

    Adding DOM event scroll, resize, toggle JS file

---
+++ DOM event scroll, resize, toggle.js
@@ -0,0 +1,41 @@
+// load, unload
+// scroll
+// resize
+// toggle --> works with details element
+
+
+//-------------load and unload--------------
+
+// window.addEventListener('load', function(){
+//     console.log("load");
+// });
+// window.addEventListener('unload', function(){
+//     console.log("unload");
+// });
+
+//----------------scroll---------------
+
+// window.addEventListener("scroll", function(){
+//     console.log("scroll");
+// });
+
+//----------------resize--------------
+
+// window.addEventListener("resize", function(){
+//     //console.log("resize");// works when we resize the window
+
+//     const width = window.outerWidth;
+//     const height = window.outerHeight;
+//     console.log(`height: ${height}, width: ${width}`);// will print the height and width of the window if we change it
+// });
+
+
+//-----------------toggle-----------------
+
+// const details = document.querySelector("details");
+
+// details.addEventListener('toggle', function(event){
+//     console.log(event.target.open);// if the summary is than it will pring true otherwise it will print false
+// });
+
+
