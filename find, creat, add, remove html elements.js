abe9875c166d8201f8eb4ed1ead5e02a5c97ba43
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-03-10 00:19:48 +0600

    Add files via upload

---
+++ find, creat, add, remove html elements.js
@@ -0,0 +1,26 @@
+
+var heading1 = document.getElementsByTagName("h1")[0];
+heading1.innerHTML = "Say hello"; // use this in console
+
+//removing element
+
+var myDiv = document.getElementById("div1");
+var heading2 = document.getElementsByTagName("h1")[1];
+myDiv.removeChild(heading2); // removing h1 (second h1)
+
+// insertBeFore()...using this we can add tags at the starting of the web page
+
+// how to find class :-
+
+//document.getElementById("id").classlist; //finding class
+//document.getElementById("id").classlist.add("new class"); // adding new class
+
+// how to style 
+
+// var link = document.getElementsByTagName("a")[0];
+// link.style.textDecoration(none);
+// link.style.color = "green";
+// link.style.fontSize = "4.5rem";
+// link.href = "www.google.com"; // changing the link
+// link.
+ 
