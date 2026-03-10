9eb39df06fdc34c1c64c99eea13e75c216285c9a
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-03-11 01:02:46 +0600

    Add files via upload

---
+++ for loop 2.js
@@ -0,0 +1,24 @@
+
+var sum = 0;
+
+for(var x=1; x<=5; x=x+1)
+{
+  sum = sum + x; //Sum of numbers from 1 to 5
+}
+document.write("Sum = "+sum);
+
+
+// same thing but taking the input from user
+
+/*var m = parseInt(prompt("Enter the starting number: "));
+var n = parseInt(prompt("Enter the starting number: "));
+var sum = 0;
+
+for(var x=m; x<=n; x=x+1)
+{
+  sum = sum + x; //Sum of numbers from 1 to 5
+}
+document.write("Sum = "+sum);
+*/
+
+
