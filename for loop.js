c79f76c0311f325214b099477db013edc2ce1dfe
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-03-12 00:14:12 +0600

    Add files via upload

---
+++ for loop.js
@@ -0,0 +1,28 @@
+
+
+for(var x = 1; x<=100; x=x+1)
+{
+    document.write(" "+ x);//  print all the numbers.
+}
+document.write("End");
+
+/*
+for(var x = 1; x<=99; x=x+2)
+{
+    document.write(" "+x ); //will print all the odd numbers within 1-100.
+}
+*/
+
+/*for(var x = 2; x<=100; x=x+2)
+{
+    document.write(" "+ x);// will print all the even numbers within 1-100.
+}*/
+
+
+/*for(var x = 10; x>=1; x=x-1)
+{
+    document.write(" "+ x);// will print 10-1.
+}
+*/
+
+
