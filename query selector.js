6de561ff7c698f90cf655c5f44b5bbda7a680b18
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-04-02 02:55:07 +0600

    Add files via upload

---
+++ query selector.js
@@ -0,0 +1,12 @@
+
+
+//document.querySelector("#p").innerHTML="This is changed";
+
+document.querySelector("a").innerHTML = "New text";
+document.querySelector("li a").innerHTML = "Tutorial changed";
+document.querySelector(".my-div a").innerHTML = "New link";
+
+
+// go to console to use this
+//document.querySelector("p")[0].innerHTML = "This is a changed paragraph"; // document.querySelector("tag")[index-number].innerHTML = "This is a changed paragraph";
+
