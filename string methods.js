fe39d4a6bd457bdc8fea38bdbe88f63ff9f41630
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-04-05 00:26:04 +0600

    Add files via upload

---
+++ string methods.js
@@ -0,0 +1,22 @@
+
+
+// startsWith(searchString, index number) -> check a string starts with another string
+
+const message1 = "Today is friday";
+console.log(message1.startsWith('Today', 0)) // if we use today it will show false
+
+// endsWith (searchString, length) -> check a string ends with another string
+
+const message2 = "Today is friday";
+console.log(message2.endsWith('friday'))
+
+// includes(searchString, index number) -> check if a string contains another string
+
+const message3 = "Today is friday";
+console.log(message3.includes('friday')) // checking if the string has the friday word
+
+// all these methods are case sensitive
+
+
+
+
