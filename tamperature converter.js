2e3193886eda8c3f29b64e5f1c011d08b9c18b03
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-04-07 00:50:47 +0600

    Add files via upload

---
+++ tamperature converter.js
@@ -0,0 +1,13 @@
+
+var farenheit = parseFloat(prompt("Enter farenheit : "));
+
+var celsius = (farenheit-32) * (5/9);
+
+document.write("Celsius = "+celsius); // Farenheit to celcius converter
+
+
+var cels = parseFloat(prompt("Enter celsius : "));
+
+var farh = (cels*(9/5)) + 32;
+
+document.write("Farenheit = "+farh);// Censius to farenheit converter
