51a0fc6734c7ef58d2c9f54251876bf445ecc059
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-04-09 02:43:08 +0600

    Add files via upload

---
+++ ternary operator.js
@@ -0,0 +1,15 @@
+
+// value > 0 ? "1st" : "2nd"
+// here if the condition is ture then it will print 1st but if it is false then it will print 2nd
+
+var number = Number(prompt("Enter a number: "));
+
+var result = number > 0 ? "P1ositive" : "Negative"
+document.write(result);
+
+//number > 0 ? document.write("Positive") : document.write("Negative");//we can write this code in one line
+
+//--------------------------------------------------------------------
+
+/*var result = number > 0 ? "Positive" : number<0 ? "Negative" : "Zero";// as like---> if, else if, else
+document.write(result);*/
