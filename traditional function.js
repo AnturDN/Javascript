c446088e2c3a81c8692cb83586d32a1a6b63d775
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-04-10 02:00:25 +0600

    Add files via upload

---
+++ traditional function.js
@@ -0,0 +1,44 @@
+
+//creating a function
+
+function square()
+{
+    var num = 5;
+    var result = num * num;
+    document.write("Result = "+ result + "<br>");
+}
+// calling a function
+
+square();
+
+
+//-------------------Parameter pass-----------------------------------------
+
+/*function square(num1, num2){
+    var result = num1 * num2;
+    document.write("Result = "+ result + "<br>");
+}
+square(10, 5);
+*/
+//-------------------Returning---------------------------------------------
+
+/*function square(num1, num2){
+    var result = num1 * num2;
+    return result;
+}
+document.write(square(5,6));
+*/
+
+//----------------- Addition and subtraction----------------------------
+
+/*function addition(x,y) {
+    var result = x + y;
+    document.write("Sum = "+ result +"<br>");
+}
+function subtraction(x,y) {
+    var result = x - y;
+    document.write("Sub = "+ result +"<br>");
+}
+addition(5,5);
+subtraction(10,5);
+*/
