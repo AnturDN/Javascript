97a797d9465a7703a1d40b5cbeb1de2ad4330be7
Antur Deb Nath <135795582+AnturDN@users.noreply.github.com>
2026-09-25 22:18:28 +0600

    Remove unnecessary line breaks in Canvas.js

--- Canvas.js
+++ Canvas.js
@@ -15,7 +15,7 @@ ctx.fillRect(12,12,376,276);// inner border
 //------Finding the center of the rectangle-----
 var centerX = c.width / 2;
 var centerY = c.height / 2;
-//-----------------------------------------------
+//---------------------------------------------
 
 ctx.beginPath();
 ctx.arc(centerX,centerY,80,0,2*Math.PI,false);//(value of x axis,value of y axis,radius,starting angle,ending angle, clockwise or counterClockwise)
